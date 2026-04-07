516b7a98f7d80ad5790f1ef166e2cf147e80aa50
HONEYGRACELATCHA <gracelatcha@gmail.com>
2026-04-07 11:36:37 +0800

    Added Component

---
+++ src/components/ExploreContainer.tsx
@@ -0,0 +1,15 @@
+import { IonButton } from '@ionic/react';
+import './ExploreContainer.css';
+
+type ContainerProps = object
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+  return (
+    <div id="container">
+       <IonButton>Default</IonButton>
+      <IonButton disabled={true}>Disabled</IonButton>
+    </div>
+  );
+};
+
+export default ExploreContainer;
