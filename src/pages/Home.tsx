e9d513852183f504538c8c3b548e737c8ee2f919
HONEYGRACELATCHA <gracelatcha@gmail.com>
2026-04-16 15:53:20 +0800

    Added tab pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,45 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonMenuButton, IonPage, IonRouterOutlet, IonTab, IonTabBar, IonTabButton, IonTabs, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import { IonReactRouter } from '@ionic/react-router';
+import { Redirect, Route } from 'react-router';
+import { bookmarkOutline, search, star } from 'ionicons/icons';
+import Feed from './home-tabs/Feed';
+import Search from './home-tabs/Search';
+import Favorites from './home-tabs/Favorites';
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          
-          <IonButtons slot="start">
-            <IonMenuButton></IonMenuButton>
-          </IonButtons>
+  const tabs = [
+    { name:"feed", tab: 'feed', url: "/app/home/feed", icon: bookmarkOutline},
+    { name:"search", tab: 'search', url: "/app/home/search", icon:search },
+    { name:"favorites", tab: 'favorites', url: '/app/home/favorites', icon:star },
+  ];
 
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
+
+  return (
+    <IonReactRouter>
+      <IonTabs>
+         <IonTabBar slot="bottom">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle>Tabs</IonTitle>
           </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
+          {tabs.map ((item, index)=>
+            <IonTabButton key={index} tab={item.tab} href={item.url}>
+              <IonIcon icon={item.icon} />
+              <IonLabel> {item.name}</IonLabel>
+            </IonTabButton>
+          )}
+         </IonTabBar>
+         <IonRouterOutlet>
+          <Route exact path="/app/home/feed" component={Feed} />
+          <Route exact path="/app/home" />
+          <Redirect to="/app/home/feed" />
+          <Route exact path="/app/home/search" component={Search} />
+          <Route exact path="/app/home/favorites" component={Favorites} />
+        </IonRouterOutlet>
+      </IonTabs>
+    </IonReactRouter>
+
   );
 };
 
---
+++ src/pages/home-tabs/Favorites.tsx
@@ -0,0 +1,23 @@
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
+
+const Favorites: React.FC = () => {
+  return (
+    <IonPage>
+       <IonHeader>
+           <IonToolbar>
+              <IonButtons>
+                  <IonMenuButton></IonMenuButton>
+                  <IonTitle>Favorites</IonTitle>
+              </IonButtons>
+           </IonToolbar>
+        </IonHeader> 
+        <IonContent className="ion-padding">
+            <h1>Favorites</h1>
+        </IonContent>
+    </IonPage>
+
+  );
+};
+
+export default Favorites;
