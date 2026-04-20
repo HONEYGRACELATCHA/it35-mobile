680886fd1d615e546260a22e68b1653c434a7dea
ALMERAJARAMILLO <jaramilloalmera92@gmail.com>
2026-04-21 06:53:59 +0800

    Added Changes

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,7 +1,7 @@
 import { IonContent, IonHeader, IonItem, IonMenu, IonPage, IonRouterOutlet, IonSplitPane, IonTitle, IonToolbar, IonIcon, IonMenuToggle, IonButton } from '@ionic/react';
 import Home from './Home';
 import { Redirect, Route } from 'react-router';
-import { homeOutline, informationOutline, settingsOutline } from 'ionicons/icons'
+import { homeOutline, informationOutline, logOutOutline, settingsOutline } from 'ionicons/icons'
 import Page1 from './Page1';
 import Page2 from './Page2';
 
@@ -35,11 +35,9 @@ const Menu: React.FC = () => {
                 </IonItem>
               </IonMenuToggle>
             ))}
-
-            <IonButton routerLink="/" routerDirection="back" expand="full">
-              Logout
-
-            </IonButton>
+             <IonButton routerLink="/" expand="full"> <IonIcon icon={logOutOutline} slot="start">
+              </IonIcon> Logout </IonButton>
+                    
           </IonContent>
 
         </IonMenu>
